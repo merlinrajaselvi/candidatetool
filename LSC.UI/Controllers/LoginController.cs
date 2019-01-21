ead9e919c4f207cd1d7b4c6c98470fba193235fb
merlinrajaselvi <46493795+merlinrajaselvi@users.noreply.github.com>
2019-01-21 13:02:16 +0530

    Add files via upload

---
+++ LSC.UI/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FizzBuzzCore.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            string username = HttpContext.User.Identity.Name;
+            return View();
+        }
+    }
+}
